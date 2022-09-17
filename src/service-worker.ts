fee6c958f84247e1c4fe8d9da7484af9e856abe5
arunkjojo <arunkjojo@gmail.com>
2022-09-17 15:39:37 +0530

    delete outdated caches

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -78,3 +78,18 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
