cbe12a9102ac33b914bef0bb54c6b0e0606bec7e
arunkjojo <arunkjojo@gmail.com>
2022-09-17 16:10:01 +0530

    service test 1

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -81,15 +81,15 @@ self.addEventListener('message', (event) => {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
+        return Promise.all(
+            cacheNames.filter(function(cacheName) {
+                // Return true if you want to remove this cache,
+                // but remember that caches are shared across
+                // the whole origin
+            }).map(function(cacheName) {
+                return caches.delete(cacheName);
+            })
+        );
     })
   );
 });
