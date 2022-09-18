081f62a9d0e35c893b8728b38bccc6781adb4b65
arunkjojo <arunkjojo@gmail.com>
2022-09-18 14:05:32 +0530

    greeting centre

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Greeting = () => {
   return (
-    <>
+    <div className='greeting center'>
       <h1>KERALA LOTTERY RESULT</h1>
       <img src="/logo_img.png" alt="LOGO" />
-    </>
+    </div>
   )
 }
 
