5a0416d48d45d55a54536c6ca573ea0e75e34fd9
arunkjojo <arunkjojo@gmail.com>
2022-09-28 23:40:44 +0530

    slider

--- src/components/MainResultCard.tsx
+++ src/components/MainResultCard.tsx
@@ -3,19 +3,19 @@ import { DrawBasicInfo } from '../interfaceTypes'
 type ResultCardProps = {
   result: DrawBasicInfo
 }
-const MainResultCard = ({result}:ResultCardProps) => {
+const MainResultCard = ({ result }: ResultCardProps) => {
   return (
     <div className="col">
       <div className="card shadow-sm">
-          
-          <div className={`bd-placeholder-img card-img-top card-heading p-1 ${result.isNewResult?'newDrawResult':''}`} id={result.id}>
-            <h2 className='drawNumber mt-2'>{result.draw}</h2>
-            <h3 className='drawName mt-2 mb-2'>{result.name}</h3>
-            <h3 className='drawDate mb-2'>{result.drawDate}</h3>
-            <h2>{result.isNewResult}</h2>
-          </div>
 
-          {/* <div className="card-body">
+        <div className={`bd-placeholder-img card-img-top card-heading p-1 ${result.isNewResult ? 'newDrawResult' : ''}`} id={result.id}>
+          <h4 className='drawNumber'>{result.draw}</h4>
+          <h4 className='drawName'>{result.name}</h4>
+          <h4 className='drawDate'>{result.drawDate}</h4>
+          <h2>{result.isNewResult}</h2>
+        </div>
+
+        {/* <div className="card-body">
           <p className="card-text"></p>
           <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
--- src/components/MainSlider.tsx
+++ src/components/MainSlider.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import Slider from './Slider'
 
 const MainSlider = () => {
     return (
         <section className="py-5 text-center slider container">
             <div className="row py-lg-5">
+                <h1 className="fw-light">Kerala Lottery Result</h1>
                 <div className="col-lg-6 col-md-8 mx-auto">
-                    <h1 className="fw-light">Kerala Lottery Result</h1>
-                    {/* <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                    <p>
-                        <a rel="noreferrer" href="/" className="btn btn-primary my-2">Main call to action</a>
-                        <a rel="noreferrer" href="/" className="btn btn-secondary my-2">Secondary action</a>
-                    </p> */}
+                    <Slider />
                 </div>
             </div>
         </section>
